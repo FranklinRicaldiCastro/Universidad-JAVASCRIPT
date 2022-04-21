d32c6dd1bf4d967d381ae80f3a9d649ecc3872c1
RICALDI CASTRO Franklin Antolin <fricaldic@gmail.com>
2022-04-20 21:29:53 -0500

    cuarto commit

---
+++ Leccion07/07-01-FuncionesJS.js
@@ -0,0 +1,17 @@
+//miFuncion(2, 2)
+
+//Declaracion de la funcion
+function miFuncion(a, b) {
+    //Imprimo el resultado
+    //console.log("Suma: " + ( a + b ));
+
+    //regresando el valor de la suma
+    return a + b;
+}
+//LLamando ala función
+let resultado = miFuncion(2, 3);
+
+console.log(resultado);
+
+//Declaracion de tipo funcion
+let x = function (a, b) {return a + b};
