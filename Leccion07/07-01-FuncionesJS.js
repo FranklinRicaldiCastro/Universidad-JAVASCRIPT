a3171fa01f78acda16dbdc6974274313c8ce4ae5
RICALDI CASTRO Franklin Antolin <fricaldic@gmail.com>
2022-04-22 00:11:16 -0500

    Commit Mejora

--- Leccion07/07-01-FuncionesJS.js
+++ Leccion07/07-01-FuncionesJS.js
@@ -5,6 +5,9 @@ function miFuncion(a, b) {
     //Imprimo el resultado
     //console.log("Suma: " + ( a + b ));
 
+    //como saber cuantos argumentos tiene mi  funcion del
+    console.log(arguments. length);
+
     //regresando el valor de la suma
     return a + b;
 }
@@ -13,5 +16,18 @@ let resultado = miFuncion(2, 3);
 
 console.log(resultado);
 
-//Declaracion de tipo funcion
-let x = function (a, b) {return a + b};
+//Declaracion la funcion de tipo Expresión
+let sumar = function (a, b) {return a + b};
+
+resultado = sumar(1, 2);
+console.log(resultado);
+
+(function (a, b) {
+    console.log('Ejecutando la función: ' + (a + b));
+})(3, 4);
+
+//tipo de dato
+console.log(typeof miFuncion);
+
+var miFuncionTexto = miFuncion.toString();
+console.log(miFuncionTexto+);
