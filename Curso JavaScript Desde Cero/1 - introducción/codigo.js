a4831f308fc925ab7cbeba7f06484e15fa7ff393
RICALDI CASTRO Franklin Antolin <fricaldic@gmail.com>
2022-05-22 21:51:41 -0500

    Aprendemos mas

---
+++ Curso JavaScript Desde Cero/1 - introducción/codigo.js
@@ -0,0 +1,75 @@
+/*
+alert("Hola")
+recipiente= "barbijo";
+alert(recipiente)
+*/
+/*
+string = "Cadena de Texto";
+number = 19;
+boolean = true;
+*/
+/*
+Let alcanse global
+var menos alcanse
+*/
+/*
+const nombre = "Dalto"
+nombre = "suarez"
+alert(nombre);
+*/
+/*
+let numero = 28;
+numero = 2;
+alert(numero);
+*/
+/*
+let numero1 = 28;
+let numero2 = 2;
+alert(numero1 + numero2);
+*/
+/*
+let nombre = prompt("Como te llamas");
+alert( "Hola " + nombre);
+*/
+/*
+let numero = 10;
+//SUMA
+// numero = numero + 5;
+numero += 5;
+document.write(numero);
+*/
+/*
+let numero = 10;
+//RESTA
+// numero = numero - 5;
+numero -= 5;
+document.write(numero);
+*/
+/*
+let numero = 10;
+//MULTIPLICACIÓN
+// numero = numero * 5;
+numero *= 5;
+document.write(numero);
+*/
+/*
+let numero = 10;
+//DIVICIÓN
+// numero = numero / 5;
+numero /= 5;
+document.write(numero);
+*/
+/*
+let numero = 10;
+//RESTO DE UNA DIVICIÓN
+// numero = numero % 5;
+numero %= 6;
+document.write(numero);
+*/
+/*
+let numero = 10;
+//EXPONENCIALISACIÓN
+// numero = numero ** 5;
+numero **= 2;
+document.write(numero);
+*/
